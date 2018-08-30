2f58130d7ca3061b068232792ae625d40098e870
QuadmasterXLII <hastings.greer@kitware.com>
2018-08-30 17:26:53 -0400

    remove unneeded print

--- Server/website/trackgyro.py
+++ Server/website/trackgyro.py
@@ -6,7 +6,6 @@ app = Flask(__name__)
 import pickle, time
 @app.route('/upload', methods=["POST"])
 def upload():
-    print(request.get_json())
     pickle.dump(request.get_json(), open("uploads/" + str(time.time()), "wb"))
     return ""
 
